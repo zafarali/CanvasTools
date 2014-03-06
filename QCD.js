cb4c560f9c7e644ead07a588c2dd17d5c468ad65
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2014-03-05 19:18:59 -0500

    made drawCircle

--- QCD.js
+++ QCD.js
@@ -1,7 +1,16 @@
 //this function creates a context object on which we can draw onto
 //the inpud is the ID of the canvas defined by <canvas id='canvasId'></canvas>
+
 var createContext = function(canvasId){
 	var canvas = document.getElementById(canvasId);
 	var context = canvas.getContext('2d');
 	return context;
+}
+
+var drawCircle = function(context, centerX, centerY, radius, thickness){
+	if(typeof(thickness)==='undefined') thickness = 2;
+	context.beginPath();
+	context.arc(centerX, centerY, radius, 0, 2*Math.PI, false);
+	context.linewidth = thickness;
+	context.stroke();
 }
